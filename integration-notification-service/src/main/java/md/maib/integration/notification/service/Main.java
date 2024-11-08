eb1502a41c3b1ecb5fe5788e4ff4a92595d4fbb1
taragrammatonn <qweqwe@qwe.qwe>
2024-11-08 10:14:19 +0200

    init commit

---
+++ integration-notification-service/src/main/java/md/maib/integration/notification/service/Main.java
@@ -0,0 +1,7 @@
+package md.maib.integration.notification.service;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ mail-notification-service/src/main/java/md/maib/mail/notification/service/Main.java
@@ -0,0 +1,7 @@
+package md.maib.mail.notification.service;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
