27a81aabede002a8ad24bfcd091df349e3b57b7e
jah3 <isecov9@gmail.com>
2024-11-11 13:51:43 +0200

    new classes for future developing

---
+++ integration-notification-service/src/main/java/md/maib/integration/notification/service/controller/NotificationController.java
@@ -0,0 +1,12 @@
+package md.maib.integration.notification.service.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+}
