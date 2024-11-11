6f0a1330437dfd6d4929cf6233e46e8b1e7f6912
jah3 <isecov9@gmail.com>
2024-11-11 13:51:43 +0200

    new classes for future developing

---
+++ integration-notification-service/src/main/java/md/maib/integration/notification/service/config/RabbitMQConfig.java
@@ -0,0 +1,8 @@
+package md.maib.integration.notification.service.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+}
---
+++ integration-notification-service/src/main/java/md/maib/integration/notification/service/model/EventModel.java
@@ -0,0 +1,12 @@
+package md.maib.integration.notification.service.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class EventModel implements Serializable {
+
+}
