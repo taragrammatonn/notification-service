509a7db18748f8a9582a3b64067192bcaf588592
jah3 <isecov9@gmail.com>
2024-11-12 09:41:30 +0200

    Add NotificationController for sending events to RabbitMQ

--- integration-notification-service/src/main/java/md/maib/integration/notification/service/controller/NotificationController.java
+++ integration-notification-service/src/main/java/md/maib/integration/notification/service/controller/NotificationController.java
@@ -1,12 +1,28 @@
 package md.maib.integration.notification.service.controller;
 
-import org.springframework.stereotype.Controller;
+import md.maib.integration.notification.service.model.EventModel;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @RestController
 @RequestMapping("/api/notifications")
 public class NotificationController {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public NotificationController(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @PostMapping("/send")
+    public String sendNotification(@RequestBody EventModel event) {
+        rabbitTemplate.convertAndSend("notifications", event);
+        return "Notification sent to RabbitMQ successfully!";
+    }
 
 }
