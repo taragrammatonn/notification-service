3d44d105b7418c19e9475641cff7b3f85f5f3c69
jah3 <isecov9@gmail.com>
2024-11-12 09:39:17 +0200

    RabbitMQ Configuration added converter

--- integration-notification-service/src/main/java/md/maib/integration/notification/service/config/RabbitMQConfig.java
+++ integration-notification-service/src/main/java/md/maib/integration/notification/service/config/RabbitMQConfig.java
@@ -1,8 +1,24 @@
 package md.maib.integration.notification.service.config;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+    @Bean
+    public Queue notificationsQueue() {
+        return new Queue("notifications");
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
 
 }
