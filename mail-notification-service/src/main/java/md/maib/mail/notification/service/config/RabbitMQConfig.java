098584f874b5f9d67cb8ccd77acc09961b738504
Mihai <potoracmihai74@gmail.com>
2024-11-25 11:31:06 +0200

    Add serializable to object convertor

--- mail-notification-service/src/main/java/md/maib/mail/notification/service/config/RabbitMQConfig.java
+++ mail-notification-service/src/main/java/md/maib/mail/notification/service/config/RabbitMQConfig.java
@@ -1,9 +1,16 @@
 package md.maib.mail.notification.service.config;
 
-
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+    
+    @Bean
+    public MessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
 }
